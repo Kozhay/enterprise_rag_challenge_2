423abb39908476ed4a8ca5dc33178461e7eedde8
Gleb Kozhaev <gleb.kozhaev@MBP-Gleb-Kozhaev.local>
2025-02-28 12:37:19 +0200

    semi

---
+++ src/answer_questions.py
@@ -0,0 +1,193 @@
+import tiktoken
+import logging
+import json
+from litellm import completion
+import csv
+from datetime import datetime
+from pdf_extractor import parse_pdf
+from pydantic import BaseModel, Field
+from typing import Union, Literal, List
+
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+def extract_pdf_pages_for_answer(json_file_path):
+    with open(json_file_path, "r", encoding="utf-8") as file:
+        data = json.load(file)
+        return data
+
+
+
+def check_labels(question, labels, file_name):
+    system_prompt = f"""
+        #ROLE: InvestGuru: Expert in Analyzing Public Company Reports
+        You are InvestGuru, a financial expert specializing in analyzing and answering questions based on public company reports.
+        Your assistant has already reviewed the reports and labeled the relevant pages.
+        #Main Task: Read the user’s QUESTION and the LABELS in the document.
+        Identify the specific pages that need to be reviewed again to accurately answer the question and commment why.
+        If you cannot answer the question based on the available data, return null to indicate this clearly.
+        #Critical Consideration
+        Your answer directly influences financial decisions, potentially leading to significant gains or losses for the user.
+        If uncertain, err on the side of inclusion rather than exclusion.
+        Accuracy is paramount—a mistake can have serious consequences.
+        """
+
+    prompt = ("QUESTION\n\n"
+              f"{question}\n\n"
+              "KNOWLEDGE DATABASE\n\n"
+              f"{labels}")
+    
+    messages = [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": prompt},
+    ]
+
+    
+    class PagesForReview(BaseModel):
+        pages: list[int]
+        comment: str
+    
+    response = completion(
+        model='gpt-4o-mini',  #groq/llama-3.3-70b-versatile
+        messages=messages,
+        response_format=PagesForReview
+    )
+
+    # Extract token usage details
+    usage = response.usage
+    prompt_tokens = usage.prompt_tokens
+    completion_tokens = usage.completion_tokens
+    total_tokens = usage.total_tokens
+    cached_tokens = usage.prompt_tokens_details.cached_tokens
+    
+    # Prepare data for CSV
+    stage = "filter_labels"
+    timestamps = datetime.now().isoformat()  # Assuming you want the current timestamp
+    file_name = file_name  # Assuming file_name is defined in the outer scope
+
+    # Write to CSV
+    with open('token_statistics.csv', mode='a', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        # Write header column names if the file is empty
+        if csv_file.tell() == 0:
+            writer.writerow(['Stage', 'Timestamp', 'File Name', 'Prompt Tokens', 'Completion Tokens', 'Total Tokens', 'Cached Tokens'])
+        writer.writerow([stage, timestamps, file_name, prompt_tokens, completion_tokens, total_tokens, cached_tokens])  # Extract cached tokens
+
+    logging.info(f"🔹 Token Usage - Prompt: {prompt_tokens}, Completion: {completion_tokens}, Total: {total_tokens}")
+
+    # Parse response content
+    response_dict = json.loads(response.choices[0].message.content)
+    return response_dict
+
+
+def answer_the_question(text, question, comment, file_name, kind):
+    system_prompt = f"""
+        ROLE: InvestGuru – Expert in Analyzing Public Company Reports
+        You are InvestGuru, a financial expert specializing in analyzing and answering questions based on public company reports.
+        #Main Task
+        You already know which pages of the report are likely to contain the answer to the user’s question.
+        Answer the user’s QUESTION using only the text from the DOCUMENT.
+        Additionally, you can refer to your own COMMENTS if they help in forming a precise answer.
+        If the available data does not allow you to answer the question, return null to indicate this clearly.
+        If answer is found return the Physical page number in the PDF file that that is answer referencing to overwise null.
+        #Critical Considerations
+        Your answer directly influences financial decisions, with potential significant gains or losses for the user.
+        Accuracy is paramount—errors can have serious consequences.
+        #Strict Rules: 
+        If the metric is an amount, extract the exact amount (e.g. if the amount in the document is given as '100 (in thousands)' or '100k', extract the value '100000'
+        You will be PENALIZED for wrong answers.
+        NEVER HALLUCINATE—base your response strictly on available data.
+        DO NOT overlook critical context.
+        """
+
+    prompt = f"""
+        QUESTION\n\n{question} with type {kind}\n\n
+        DOCUMENT named pdf_sha1={file_name}\n\n{text}\n\n
+        COMMENT\n\n{comment}
+        """
+    
+
+    messages = [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": prompt},
+    ]
+    
+    class SourceReference(BaseModel):
+        page_index: int = Field(..., description="Physical page number in the PDF file")
+    
+    class Answer(BaseModel):
+        question_text: str = Field(..., description="Text of the question")
+        kind: Literal["number", "name", "boolean", "names"] = Field(..., description="Kind of the question")
+        value: Union[float, str, bool, List[str], Literal["N/A"]] = Field(..., description="Answer to the question, according to the question schema")
+        references: List[SourceReference]
+
+    
+    response = completion(
+        model='gpt-4o-mini',  #groq/llama-3.3-70b-versatile
+        messages=messages,
+        response_format=Answer
+    )
+
+    # Extract token usage details
+    usage = response.usage
+    prompt_tokens = usage.prompt_tokens
+    completion_tokens = usage.completion_tokens
+    total_tokens = usage.total_tokens
+    cached_tokens = usage.prompt_tokens_details.cached_tokens
+    
+    # Prepare data for CSV
+    stage = "answer_question"
+    timestamps = datetime.now().isoformat()  # Assuming you want the current timestamp
+    file_name = file_name  # Assuming file_name is defined in the outer scope
+
+    # Write to CSV
+    with open('token_statistics.csv', mode='a', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        # Write header column names if the file is empty
+        if csv_file.tell() == 0:
+            writer.writerow(['Stage', 'Timestamp', 'File Name', 'Prompt Tokens', 'Completion Tokens', 'Total Tokens', 'Cached Tokens'])
+        writer.writerow([stage, timestamps, file_name, prompt_tokens, completion_tokens, total_tokens, cached_tokens])  # Extract cached tokens
+
+    logging.info(f"🔹 Token Usage - Prompt: {prompt_tokens}, Completion: {completion_tokens}, Total: {total_tokens}")
+
+
+    # Parse response content
+    response_dict = json.loads(response.choices[0].message.content)
+    return response_dict
+
+
+
+TOKENIZER = tiktoken.encoding_for_model("gpt-4o-mini")
+file_name='0279901b645e568591ad95dac2c2bf939ef0c00d'
+company_name = "ACRES Commercial Realty Corp"
+major_industry = "Financial Services"
+pdf_path = f"examples/pdfs/{file_name}.pdf"
+
+questions = [{
+    "text": "Did ACRES Commercial Realty Corp. outline any new ESG initiatives in the annual report? If there is no mention, return False.",
+    "kind": "boolean"
+  }]
+
+
+lables_json_file_path = f"output/labels/_{file_name}.json"  # Replace with your actual JSON file path
+labeled_pages_list = extract_pdf_pages_for_answer(lables_json_file_path)
+
+
+for question in questions:
+    question_text = question['text']
+    question_kind = question['kind']
+    labels_for_the_questions = check_labels(question=question_text, labels=labeled_pages_list, file_name=file_name)
+    pages = labels_for_the_questions['pages']
+    comment = labels_for_the_questions['comment']
+    logging.info(f'Pages for context: {pages}. Comment - {comment}')
+    pdf_parsed_texts = parse_pdf(pdf_path=pdf_path, pages=pages, max_tokens=100_000)
+    answer = answer_the_question(text=pdf_parsed_texts, question=question_text, comment=comment, kind=question_kind, file_name=file_name)
+    print(answer)
+
+
+
+# structured_datas = []
+# for pdf_text in pdf_parsed_texts:
+#     extracted = answered_document_content(pdf_text['text'], industry_type=major_industry)
+#     structured_datas.append(extracted)
--- src/pdf_extractor.py
+++ src/pdf_extractor.py
@@ -2,19 +2,18 @@
 import pymupdf4llm
 import tiktoken
 import logging
-import re
 import json
 import os
 from litellm import completion
 import models
+import csv
 
-from pydantic import BaseModel, field_validator
-from typing import Optional, Union, Literal
-from enum import Enum
+from pydantic import BaseModel
+from datetime import datetime
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
-
+TOKENIZER = tiktoken.encoding_for_model("gpt-4o-mini")
 
 def count_tokens(text):
     """Counts the number of tokens in a given text."""
@@ -63,7 +62,6 @@ def parse_pdf(pdf_path, pages=None, max_tokens=500):
     
     # Merge pages while keeping page numbers inside the text
     merged_chunks = merge_chunks_with_pages(page_chunks, max_tokens)
-    print(merged_chunks)
 
     logging.info(f"🎯 Total merged chunks created: {len(merged_chunks)}")
     return merged_chunks
@@ -74,7 +72,7 @@ def parse_pdf(pdf_path, pages=None, max_tokens=500):
 
 import json
 
-def extract_document_content(text, industry_type='financial_services'):
+def extract_document_content(text, industry_type, file_name):
     system_prompt = f"""
         You are an assistant with access to a long document and a list of searchable items.
         You are analyzing a document from the {industry_type} industry.
@@ -131,12 +129,26 @@ def extract_document_content(text, industry_type='financial_services'):
         response_format=DocumentContent
     )
 
+    
     # Extract token usage details
     usage = response.usage
     prompt_tokens = usage.prompt_tokens
     completion_tokens = usage.completion_tokens
     total_tokens = usage.total_tokens
-    #cached_tokens = usage.prompt_tokens_details.cached_tokens  # Extract cached tokens
+    cached_tokens = usage.prompt_tokens_details.cached_tokens
+    
+    # Prepare data for CSV
+    stage = "labeling"
+    timestamps = datetime.now().isoformat()  # Assuming you want the current timestamp
+    file_name = file_name  # Assuming file_name is defined in the outer scope
+
+    # Write to CSV
+    with open('token_statistics.csv', mode='a', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        # Write header column names if the file is empty
+        if csv_file.tell() == 0:
+            writer.writerow(['Stage', 'Timestamp', 'File Name', 'Prompt Tokens', 'Completion Tokens', 'Total Tokens', 'Cached Tokens'])
+        writer.writerow([stage, timestamps, file_name, prompt_tokens, completion_tokens, total_tokens, cached_tokens])  # Extract cached tokens
 
     logging.info(f"🔹 Token Usage - Prompt: {prompt_tokens}, Completion: {completion_tokens}, Total: {total_tokens}")
 
@@ -144,71 +156,71 @@ def extract_document_content(text, industry_type='financial_services'):
     response_dict = json.loads(response.choices[0].message.content)
     return response_dict
 
-    # return {
-    #     "data": response_dict,
-    #     "token_usage": {
-    #         "prompt_tokens": prompt_tokens,
-    #         "completion_tokens": completion_tokens,
-    #         "total_tokens": total_tokens,
-    #         "cached_tokens": cached_tokens  # Include cached tokens in return
-    #     }
-    # }
 
 
+def main():
+    with open('examples/subset.json', "r") as f:
+        data = json.load(f)
+        for company in data:
+            file_name = company['sha1']
+            company_name = company['company_name']
+            major_industry = company['major_industry']
+            logging.info(f"Processing company: {company_name} with SHA1: {file_name} and industry: {major_industry}")
 
+            # Set tokenizer model (e.g., "gpt-4" or "gpt-3.5-turbo")
+            pdf_path = f"examples/pdfs/{file_name}.pdf"
+            logging.info(f"PDF path set to: {pdf_path}")
 
- # Set tokenizer model (e.g., "gpt-4" or "gpt-3.5-turbo")
-TOKENIZER = tiktoken.encoding_for_model("gpt-4o-mini")
-file_name='0279901b645e568591ad95dac2c2bf939ef0c00d'
-company_name = "ACRES Commercial Realty Corp"
-major_industry = "Financial Services"
+            # Parse PDF
+            # Generate list of pages to parse from 0 to 5
+            pages_to_parse = list(range(0, 5))  # End number is exclusive
+            logging.info(f"Pages to parse: {pages_to_parse}")
 
+            pdf_parsed_texts = parse_pdf(pdf_path, pages=pages_to_parse, max_tokens=100_000)
+            logging.info(f"Parsed {len(pdf_parsed_texts)} pages from the PDF.")
 
+            # Extract structured data
+            structured_datas = []
+            for pdf_text in pdf_parsed_texts:
+                extracted = extract_document_content(pdf_text['text'], industry_type=major_industry, file_name=file_name)
+                structured_datas.append(extracted)
+                #logging.info(f"Extracted data from PDF text: {extracted}")
 
-pdf_path = f"examples/pdfs/{file_name}.pdf"
+            merged_data_points = []
+            merged_events = []
 
-# Parse PDF
-# Generate list of pages to parse from 14 to 15
-pages_to_parse = list(range(0, 5))  # End number is exclusive
+            # Iterate through structured_datas and merge the 'data_points'
+            for data in structured_datas:
+                merged_data_points.extend(data['data_points'])
+                merged_events.extend(data['events'])
+                logging.info(f"Merged data points: {len(merged_data_points)}, Merged events: {len(merged_events)}")
 
-pdf_parsed_texts = parse_pdf(pdf_path, pages=pages_to_parse, max_tokens=100_000)
+            final_output = {
+                'company_name': company_name,
+                'file_name': file_name,
+                'data_points': merged_data_points,
+                'events': merged_events
+            }
 
-# Extract structured data
-structured_datas = []
-for pdf_text in pdf_parsed_texts:
-    pass
-    extracted = extract_document_content(pdf_text['text'], industry_type=major_industry)
-    structured_datas.append(extracted)
+            # Print the final output
+            logging.info(f"Final output for {company_name}: {final_output}")
 
-merged_data_points = []
-merged_events = []
+            # Define the output folder
+            output_folder = "output/labels"
 
-# Iterate through structured_datas and merge the 'data_points'
-for data in structured_datas:
-    merged_data_points.extend(data['data_points'])
-    merged_events.extend(data['events'])
+            # Ensure the output folder exists, create it if it doesn't
+            if not os.path.exists(output_folder):
+                os.makedirs(output_folder)
+                logging.info(f"Created output folder: {output_folder}")
 
-final_output = {
-    'company_name': file_name,
-    'data_points': merged_data_points,
-    'events': merged_events
-}
+            # Define the output file path with the company name as the filename
+            output_file_path = os.path.join(output_folder, f"{file_name}.json")
+            logging.info(f"Output file path: {output_file_path}")
 
-# Print the final output
-print(final_output)
+            # Write final_output to the file in JSON format
+            with open(output_file_path, 'w', encoding='utf-8') as output_file:
+                json.dump(final_output, output_file, ensure_ascii=False, indent=4)
+                logging.info(f"Written final output to {output_file_path}")
 
-# Define the output folder
-output_folder = "output/labels"
-
-# Ensure the output folder exists, create it if it doesn't
-if not os.path.exists(output_folder):
-    os.makedirs(output_folder)
-
-# Define the output file path with the company name as the filename
-output_file_path = os.path.join(output_folder, f"{file_name}.json")
-
-# Write final_output to the file in JSON format
-with open(output_file_path, 'w') as json_file:
-    json.dump(final_output, json_file, indent=4)
-
-print(f"File saved to {output_file_path}")
+if __name__ == "__main__":
+    main()
